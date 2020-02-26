db18058799d8e3f7b601eaeb6b850d1eca93b9d0
Reyna31 <53918885+Reyna31@users.noreply.github.com>
2020-02-25 22:07:42 -0600

    Add files via upload

---
+++ APila.java
@@ -0,0 +1,12 @@
+
+
+public abstract class APila<T> implements IPila<T> {
+    public void add(T value){
+        add(value);
+    }
+
+    public T remove(){
+        return remove();
+    }
+
+}
---
+++ IPila.java
@@ -0,0 +1,11 @@
+
+
+public interface IPila<T> {
+
+public void add(T value);
+
+public T remove();
+
+public T peek();
+
+}
